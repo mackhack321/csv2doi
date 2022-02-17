4966dcd5b8eba15ea9fb05681382977c9fabe062
Mack Stanley <mlgprohacker@gmail.com>
2022-02-17 16:24:03 -0600

    make navbar actually use the fast routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "@reach/router";
 import React from "react";
 // import logo from "./horz_maroon.svg";
 import logo from "./csv2doi.png";
@@ -6,6 +7,25 @@ export default function Navbar() {
   return (
     <div>
       <img src={logo} alt="logo" className="h-24" />
+      <div className="flex space-x-4 px-5 pb-3">
+        <Link to="/">
+          <button
+            className="px-3 py-2 bg-msugreen text-white rounded-md font-medium hover:bg-green-800"
+            type="button"
+          >
+            Home
+          </button>
+        </Link>
+
+        <Link to="about">
+          <button
+            className="px-3 py-2 bg-msugreen text-white rounded-md font-medium hover:bg-green-800"
+            type="button"
+          >
+            About
+          </button>
+        </Link>
+      </div>
       <div className="bg-maroon h-1"></div>
     </div>
   );
