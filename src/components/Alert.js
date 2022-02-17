51ca8f6440aa1853b72375ded94d38ec34b30a4c
Mack Stanley <mlgprohacker@gmail.com>
2022-02-17 12:52:44 -0600

    make custom alert pulse animation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    <div className="bg-red-500 rounded-md w-fit px-2 py-4 text-white font-medium">
+    <div className="animate-pulse bg-red-500 rounded-md w-fit px-2 py-4 text-white font-medium">
       <p>There was a problem executing your request:</p>
       <p>{props.message}</p>
     </div>
