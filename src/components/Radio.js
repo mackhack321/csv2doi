5530b41804a4d260764a7509757fdb43bb0a2e2e
Mack Stanley <mlgprohacker@gmail.com>
2022-04-11 14:42:20 -0500

    change radio buttons to dropdown

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -12,43 +12,11 @@ export default function Radio() {
           Select Datatype:
         </label>
 
-        <div className="justify-center ">
-          {/* <h1>Radio value is : {radio}</h1> */}
-          <input
-            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-            type="radio"
-            checked={radio === "dissertation"}
-            value="dissertation"
-            onChange={(e) => {
-              setRadio(e.target.value);
-            }}
-          />
-          <label className="pl-2 font-medium text-gray-700">Dissertation</label>
-          <br />
-          <input
-            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-            type="radio"
-            checked={radio === "dataset"}
-            value="dataset"
-            onChange={(e) => {
-              setRadio(e.target.value);
-            }}
-          />
-          <label className="pl-2 font-medium text-gray-700">Dataset</label>
-          <br />
-          <input
-            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-            type="radio"
-            checked={radio === "refUpdate"}
-            value="refUpdate"
-            onChange={(e) => {
-              setRadio(e.target.value);
-            }}
-          />
-          <label className="pl-2 font-medium text-gray-700">
-            Reference Update
-          </label>
-        </div>
+        <select className="w-fit" onChange={(e) => setRadio(e.target.value)}>
+          <option value="dissertation">Dissertation</option>
+          <option value="dataset">Dataset</option>
+          <option value="refUpdate">Reference Update</option>
+        </select>
       </form>
       {radio === "dataset" && <Dataset />}
       {radio === "dissertation" && <Dissertation />}
